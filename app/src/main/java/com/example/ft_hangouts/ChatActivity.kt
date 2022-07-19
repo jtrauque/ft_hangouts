cb97a61178a4f3a940ddecc376a707f4d58fb91c
WebForce3 <webforce3@c1r6p6.42quebec.com>
2022-07-19 15:29:01 -0400

    end of the day

--- app/src/main/java/com/example/ft_hangouts/ChatActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/ChatActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,22 +15,24 @@ class ChatActivity : AppCompatActivity() {
     private lateinit var sendButton: ImageView
     private lateinit var messageAdapter: MessageAdapter
     private lateinit var messageList: ArrayList<Message>
-    lateinit var sqliteHelper: DataBaseHandler
+    private lateinit var sqliteHelper: DataBaseHandler
 
-    val receiverRoom: Int = 0
-    val senderRoom: Int = 0
+    var receiverRoom: Int = 0
+    var senderRoom: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
+        sqliteHelper = DataBaseHandler(this)
+        val senderId: Int = 0 // a lier avec le base de donnee
+
+     //   var text: TextView = findViewById(R.id.btnView)
         val name = intent.getStringExtra("name")
         val receiverId = intent.getIntExtra("id", 0)
 
-        sqliteHelper = DataBaseHandler(this)
-        val senderId: Int = 0 // a lier avec le base de donnee
-        // senderRoom = receiverId + senderId
-        // receiverRoom = senderId + receiverId
+       // senderRoom = receiverId + senderId
+       // receiverRoom = senderId + receiverId
         supportActionBar?.title = name //to have the receiver name on top of your screen
 
         messageRecyclerView = findViewById(R.id.messageRecycleView)
@@ -38,11 +41,14 @@ class ChatActivity : AppCompatActivity() {
         messageList = ArrayList()
         messageAdapter = MessageAdapter(this, messageList)
 
+        //add data to recycleView
+
         sendButton.setOnClickListener {
             val message = messageBox.text.toString()
             val messageObject = Message(message, senderId, receiverId)
 
             sqliteHelper.newMessage(messageObject)
+            messageBox.setText("")
         }
     }
 }
