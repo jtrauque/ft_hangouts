7d273dbb41743e353912925b1b63b6193a1fc724
Julie Trauque <jtrauque@c1r6p6.42quebec.com>
2022-08-08 19:55:57 -0400

    Ongoing : language selection - syntax issues ?

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.ft_hangouts
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import android.text.Html
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -24,6 +30,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     private lateinit var btnAdd:ImageButton
     private lateinit var btnSms:ImageButton
     private lateinit var btnSet:ImageButton
+    private lateinit var btnLanguage:ImageButton
     private lateinit var title : TextView
     private lateinit var tape : LinearLayout
 
@@ -34,6 +41,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loadLocate()
         setContentView(R.layout.activity_main)
 
         sqliteHelper = DataBaseHandler(this)
@@ -54,6 +62,9 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             val intent = Intent(this, PopUpColor::class.java)
             startActivity(intent)
         }
+        btnLanguage.setOnClickListener(){
+            showChangeLang()
+        }
         adapter?.setOnClickItem {
             Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
             ct = it
@@ -64,6 +75,61 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
+    private fun showChangeLang() {
+        val listItems = arrayOf("English", "French")
+
+        val mBuilder = AlertDialog.Builder(this)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItems, -1) { dialog, which ->
+            Log.e("LANGUAGE :", which.toString())
+            if (which == 0) {
+                setLocate("en")
+                recreate()
+            } else if (which == 1) {
+                setLocate("fr-rFR")
+                recreate()
+            }
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+
+    private fun setLocate(Lang: String) {
+        val local = Locale(Lang)
+        Locale.setDefault(local)
+        val config = Configuration()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            config.setLocale(local)
+
+            val localeList = LocaleList(local)
+            LocaleList.setDefault(localeList)
+            config.setLocales(localeList)
+        } else {
+            config.locale = local
+            config.setLocale(local)
+        }
+//        config.setLocale(local)
+       // baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            config.setLayoutDirection(local)
+            this.createConfigurationContext(config)
+        } else {
+            this.resources.updateConfiguration(config, this.resources.displayMetrics)
+        }
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        Log.e("SETLOCATE ", editor.putString("My_Lang", Lang).toString())
+        editor.apply()
+    }
+
+    private fun loadLocate() {
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val language = sharedPreferences.getString("My_Lang", "")
+        Log.e("LOADLOCATE ", language.toString())
+        setLocate(language!!)
+    }
+
     private fun activityColor() {
         var colorText = ColorManager.text
         title.setBackgroundColor(Color.parseColor(ColorManager.back))
@@ -72,6 +138,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         tape.setBackgroundColor(Color.parseColor(ColorManager.back))
         btnSet.setColorFilter(Color.parseColor(colorText))
         btnSms.setColorFilter(Color.parseColor(colorText))
+        btnLanguage.setColorFilter(Color.parseColor(colorText))
     }
 
     public override fun onStart() {
@@ -154,6 +221,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
         btnAdd = findViewById<ImageButton>(R.id.btnAdd)
         btnSms = findViewById<ImageButton>(R.id.btnConv)
         btnSet = findViewById<ImageButton>(R.id.btnSet)
+        btnLanguage = findViewById<ImageButton>(R.id.btnLanguage)
         title = findViewById<TextView>(R.id.btnView)
         tape = findViewById<LinearLayout>(R.id.tape)
         recyclerView = findViewById((R.id.recycleView))
